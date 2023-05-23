f99beee0a021f4f6a1bb597b0609702294fb3ddc
sooryasureshbabu <52371029+sooryasureshbabu@users.noreply.github.com>
2023-05-23 13:45:53 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+#  functions & return
+def mul(x,y):
+    return x,y
+   
+print(mul(1,3))  
+
+# function call
+def soo():
+    print('learning python programming')
+soo() 
+
+# argument is a value that is passed to the function when it is called.
+# *args 
+def numbers(*args):
+    return args
+print(numbers(1,2,3,4,5))    
